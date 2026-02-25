4434a897c70a65c1749c57f0dff829095b89f539
Leroy Ramaphoko <adventapp777@gmail.com>
2026-02-26 00:16:14 +0200

    Initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
+import com.google.protobuf.gradle.*
+
 plugins {
     kotlin("jvm") version "2.1.0"
     `maven-publish`
+    id("com.google.protobuf") version "0.9.4"
 }
 
-group = "com.biblecontentmedia.dailyverse.proto"
-version = "1.0.0"
+group = "com.biblecontentmedia.dailyverse.protos"
+version = "1.0.3"
 
 repositories {
     mavenCentral()
@@ -16,6 +19,7 @@ dependencies {
     api("io.grpc:grpc-protobuf:1.69.0")
     api("javax.annotation:javax.annotation-api:1.3.2")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    testImplementation(kotlin("test"))
 }
 
 kotlin {
@@ -26,6 +30,31 @@ java {
     withSourcesJar()
 }
 
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.25.5"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.69.0"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.4.1:jdk8@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+            task.builtins {
+                id("kotlin")
+            }
+        }
+    }
+}
+
 publishing {
     publications {
         create<MavenPublication>("maven") {
@@ -33,4 +62,8 @@ publishing {
             artifactId = "daily-verse-proto"
         }
     }
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
